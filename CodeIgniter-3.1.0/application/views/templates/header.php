512090da399befa8c7e9edeb7df95fe539b76330
khafidzdev <khafidzrizziq@gmail.com>
2025-09-27 11:14:41 +0700

    belum ada login

--- CodeIgniter-3.1.0/application/views/templates/header.php
+++ CodeIgniter-3.1.0/application/views/templates/header.php
@@ -3,26 +3,69 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title ?? 'Sistem Laporan' ?></title>
+    <title><?= $title ?> - Koperasi Merdeka</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <style>
+        .sidebar {
+            min-height: 100vh;
+            background-color: #343a40;
+        }
+        .sidebar .nav-link {
+            color: #fff;
+        }
+        .sidebar .nav-link:hover {
+            background-color: #495057;
+        }
+        .content {
+            padding: 20px;
+        }
+    </style>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="<?= base_url() ?>">
-                <i class="fas fa-chart-bar"></i> Sistem Laporan
-            </a>
-            
-            <?php if ($this->session->userdata('logged_in')): ?>
-            <div class="navbar-nav ms-auto">
-                <span class="navbar-text me-3">
-                    <i class="fas fa-user"></i> <?= $this->session->userdata('nama_lengkap') ?>
-                </span>
-                <a class="nav-link" href="<?= base_url('auth/logout') ?>">
-                    <i class="fas fa-sign-out-alt"></i> Logout
-                </a>
-            </div>
-            <?php endif; ?>
-        </div>
-    </nav>
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Sidebar -->
+            <nav class="col-md-3 col-lg-2 d-md-block sidebar collapse">
+                <div class="position-sticky pt-3">
+                    <h4 class="text-white text-center">Koperasi Merdeka</h4>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url() ?>">
+                                <i class="fas fa-home"></i> Dashboard
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url('barang') ?>">
+                                <i class="fas fa-box"></i> Data Barang
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url('jenis-barang') ?>">
+                                <i class="fas fa-tags"></i> Jenis Barang
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url('barang-masuk') ?>">
+                                <i class="fas fa-arrow-down"></i> Barang Masuk
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url('barang-keluar') ?>">
+                                <i class="fas fa-arrow-up"></i> Barang Keluar
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url('laporan') ?>">
+                                <i class="fas fa-chart-bar"></i> Laporan
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <!-- Main content -->
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 content">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 class="h2"><?= $title ?></h1>
+                </div>
